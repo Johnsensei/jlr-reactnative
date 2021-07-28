3a19944cb253c6015039706fc22cb316e1cee687
Johnsensei <john@johnsensei.com>
2021-07-27 20:17:14 -0500

    Adds custom drawer image

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,11 +6,12 @@ import Contact from './ContactComponent';
 import Apps from './AppsComponent';
 import Videos from './VideosComponent';
 import Constants from 'expo-constants';
-import { View, Platform, StyleSheet } from 'react-native';
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+import SafeAreaView from 'react-native-safe-area-view';
 
 const HomeNavigator = createStackNavigator(
     {
@@ -128,6 +129,21 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const CustomDrawerContentComponent = props => (
+    <ScrollView>
+        <SafeAreaView 
+            style={styles.container}
+            forceInset={{top: 'always', horizontal: 'never'}}>
+            <View style={styles.drawerHeader}>
+                <View style={{flex: 1}}>
+                    <Image source={require('../shared/images/JLR_Vert.png')} style={styles.drawerImage} />
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
+    </ScrollView>
+);
+
 const MainNavigator = createDrawerNavigator(
     {
         "Home": { screen: HomeNavigator,
@@ -187,7 +203,8 @@ const MainNavigator = createDrawerNavigator(
             } }
     },
     {
-        drawerBackgroundColor: '#7A5C4A'
+        drawerBackgroundColor: '#7A5C4A',
+        contentComponent: CustomDrawerContentComponent
     }
 );
 
@@ -207,6 +224,28 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#7A5C4B',
+        height: 140,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        marginLeft: 40,
+        marginBottom: 10,
+        height: 100,
+        width: 180
+    },
     stackIcon: {
         marginLeft: 10,
         color: 'black',
