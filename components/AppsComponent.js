95f2e256f2ecd932fc23bf2a8fe2adbfbb1e9892
Johnsensei <john@johnsensei.com>
2021-07-27 19:15:47 -0500

    Apps Component Working

--- components/AppsComponent.js
+++ components/AppsComponent.js
@@ -1,50 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Card, FlatList } from 'react-native-elements';
+import { FlatList, TouchableOpacity } from 'react-native';
+import { ListItem, Card, Text } from 'react-native-elements';
 import { APPS } from '../shared/apps';
-
-function RenderItem ({item}){
-    if (item){
-        if(item.description){item.description=item.description}
-        else{item.description="No description"}
-        return(
-            <Card
-                
-                image={require('./images/hb-placeholder.png')}
-            >
-                <Text style={{margin: 10}}>
-                    {item.description}
-                </Text>
-            </Card>
-        );
-    }
-    return <View><Text>No item.</Text></View>
-}
 
 class Apps extends Component {
     constructor(props){
         super(props);
         this.state = {
             apps: APPS
-        }
+        };
     }
-
+    
     static navigationOptions = {
         title: "Apps"
-    }
-
+    };
+    
     render(){
-        return(
-        //     <FlatList
-        //     data={this.state.languageClasses}
-        //     renderItem={RenderItem}
-        //     keyExtractor={item => item.id.toString()}
-        // />
-        <RenderItem 
-            item={this.state.apps}
-        />
+        const { navigate } = this.props.navigation;
+        const renderAppsItem = ({item}) => {
+            if(item){
+                return (
+                    <Card
+                        image={item.image}>
+                        <Text style={{margin: 10}}>
+                            {item.description}
+                        </Text>
+                    </Card>
+
+                );
+            }
+            return <View><Text>No item.</Text></View>
+        };
+
+        return (
+            <FlatList
+                data={this.state.apps}
+                renderItem={renderAppsItem}
+                keyExtractor={item => item.id.toString()}
+            />
         );
-    }
+    };
 }
 
 export default Apps;
